1024aa6a8974d88b208db79030b92061a710b42b
vic30004 <victor_993@msn.com>
2020-04-01 11:59:30 -0700

    added the variables

---
+++ app.js
@@ -0,0 +1,6 @@
+const main = document.getElementById('main');
+const addUserBtn = document.getElementById('add-user');
+const doubleBtn = document.getElementById('double');
+const showMillionairsBtn = document.getElementById('show-millionairs');
+const sortBtn = document.getElementById('sort');
+const calculateWealthBtn = document.getElementById('calculate-wealth');
