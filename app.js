d39fa2366babacb1c0ea8a47d11ae3fa191b76fb
vic30004 <victor_993@msn.com>
2020-04-01 12:06:41 -0700

    added the function to add the names to the array

--- app.js
+++ app.js
@@ -5,27 +5,28 @@ const showMillionairsBtn = document.getElementById('show-millionairs');
 const sortBtn = document.getElementById('sort');
 const calculateWealthBtn = document.getElementById('calculate-wealth');
 
-
 let data = [];
 
 // Fetch random users and add money
-getRandomUser()
-getRandomUser()
-getRandomUser()
+getRandomUser();
+getRandomUser();
+getRandomUser();
 
+async function getRandomUser() {
+  const URL = 'https://randomuser.me/api';
+  const res = await fetch(URL);
+  const data = await res.json();
 
-async function getRandomUser(){
-    const URL = "https://randomuser.me/api";
-    const res = await fetch(URL);
-    const data = await res.json();
-    
-    const user = data.results[0];
+  const user = data.results[0];
 
-    const newUser = {
-        name: `${user.name.first} ${user.name.last}`,
-        money: Math.floor(Math.random()*1000000)
-    }
-    addData(newUser)
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    money: Math.floor(Math.random() * 1000000)
+  };
+  addData(newUser);
 }
 
-
+// Add new Obj to data arr
+function addData(obj) {
+  data.push(obj);
+}
