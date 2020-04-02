9236bfb39814191e2fc9c6f420cc440b9a75ad2c
vic30004 <victor_993@msn.com>
2020-04-01 18:18:14 -0700

    added a function to format money as currency string

--- app.js
+++ app.js
@@ -41,7 +41,13 @@ function updateDom(providedData=data){
     providedData.forEach(item=>{
         const element = document.createElement('div');
         element.classList.add('person')
-        element.innerHTML=`<strong>${item.name}</strong>${item.money}`
+        element.innerHTML=`<strong>${item.name}</strong>$${formatMoney(item.money)}`
         main.appendChild(element);
     })
+}
+
+// Format number as money
+function formatMoney(number){
+ return (number).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');  // 12,345.67
+
 }
